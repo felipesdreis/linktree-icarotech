b25a9e7216ea818c5aef4a9e827dfdefa279163b
Felipe Reis <32417584+felipesdreis@users.noreply.github.com>
2023-09-28 18:03:31 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const shareButttons = document.querySelectorAll('.tile-share-button')
+
+async function copyText(e){
+    //previnir de fazer o default que é ir para o site
+    e.preventDefault()
+    let link = this.getAttribute('link')
+    try {
+        await navigator.clipboard.writeText(link)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+shareButttons.forEach(
+    button => button.addEventListener('click', copyText)
+)
